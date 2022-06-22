7f8a6f942b96a7ada4ef338a124440dd78adbdf8
Kyryl Fedko <kfedko@ascendix.net>
2022-06-22 13:41:55 +0200

    add Tag repository, connect with db

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TagEntity } from './tag.entity';
 
 @Injectable()
 export class TagService {
-  findAll(): string[] {
-    return ['rc', 'alpha', 'beta'];
+  constructor(
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
+  ) {}
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagRepository.find();
   }
 }
