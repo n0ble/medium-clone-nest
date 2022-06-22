330531338e74b51cf7575f8200ac5aa9bee431c3
Kyryl Fedko <kfedko@ascendix.net>
2022-06-22 09:52:55 +0200

    add TagService

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { TagService } from './tag.service';
 
 @Controller('tags')
 export class TagController {
+  constructor(private readonly tagService: TagService) {}
+
   @Get()
   findAll() {
-    return ['rc', 'alpha', 'beta'];
+    return this.tagService.findAll();
   }
 }
